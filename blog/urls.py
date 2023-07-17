5518e76d98a94e98a3152a48b153d5b0e56334fb
25th-Night <brown_eyed87@naver.com>
2023-07-17 15:11:15 +0900

    blog/urls.py - 생성한 뷰 함수를 urlpatterns에 등록

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from blog.views import *
+from . import views
+
+app_name = 'blog'
 
 urlpatterns = [
-
+    # post views
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
 ]
