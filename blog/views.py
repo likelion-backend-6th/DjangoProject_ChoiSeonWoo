6ffc219916bc59d0db0fae0380ae16883f514b4d
25th-Night <brown_eyed87@naver.com>
2023-07-19 11:31:55 +0900

    post_list : EmptyPage 예외 처리

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
@@ -6,9 +6,16 @@ from .models import Post
 def post_list(request):
     post_list = Post.published.all()
     # 페이지당 3개의 게시물로 페이지네이션
-    paginator = Paginator(post_list, 3)
+    per_page = request.GET.get('per_page', 3)
+    paginator = Paginator(post_list, per_page)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # page_number가 범위를 벗어난 경우 결과의 마지막 페이지 제공
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
