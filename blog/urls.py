521cce4966e5ca348639999d6da5d77843ac29ec
25th-Night <brown_eyed87@naver.com>
2023-07-19 09:50:16 +0900

    post_detail - url path를 정규식 패턴을 사용한 re_path 함수로 작성

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'blog'
@@ -6,7 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+    re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$',
          views.post_detail,
          name='post_detail'),
 ]
