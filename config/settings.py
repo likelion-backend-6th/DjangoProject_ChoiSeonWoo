cb9f3308b3057981d747c8ea6cc0fd9a25df6529
25th-Night <brown_eyed87@naver.com>
2023-07-21 14:01:59 +0900

    settings.py - postgresql DB 설정

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,8 @@ import os
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,6 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# .env 파일 로드
+load_dotenv()
 
 # MEDIA 경로
 MEDIA_URL = "/media/"
@@ -101,10 +105,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'NAME': os.getenv("POSTGRES_DBNAME"),
+        'PORT': os.getenv("POSTGRES_PORT"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
