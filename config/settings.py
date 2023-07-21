e7fadf98e1e230f3284f23a619e5939299922774
25th-Night <brown_eyed87@naver.com>
2023-07-21 14:02:54 +0900

    settings.py - INSTALLED_APPS에 'django.contrib.postgres' 추가

--- config/settings.py
+++ config/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
 
     # Thrid-party Apps
     'taggit',
